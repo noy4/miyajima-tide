f59ec2e4f88141096de9a485a1a78c8990cc6905
noy4 <naoyan418@gmail.com>
2025-05-15 19:29:13 +0900

    pages

---
+++ src/routes/date-data-json/+page.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+  import { WeatherState } from '$lib/state.svelte'
+
+  const state = new WeatherState()
+</script>
+
+<pre>
+  {JSON.stringify(state.data, null, 2)}
+</pre>
---
+++ src/routes/pages/+page.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+  const items = [
+    '/json',
+    '/date-data-json',
+    '/table',
+    '/table-vertical',
+  ]
+</script>
+
+{#each items as v}
+  <div>
+    <a href={v}>{v}</a>
+  </div>
+{/each}
