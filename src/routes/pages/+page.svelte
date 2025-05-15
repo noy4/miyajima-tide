2fd66fa31b6a0587e7924df24894d9dc079a6f6d
noy4 <naoyan418@gmail.com>
2025-05-15 20:48:15 +0900

    base

--- src/routes/pages/+page.svelte
+++ src/routes/pages/+page.svelte
@@ -1,10 +1,12 @@
 <script lang='ts'>
+  import { base } from '$app/paths'
+
   const items = [
     '/data',
     '/date-data',
     '/table',
     '/table-vertical',
-  ]
+  ].map(v => `${base}${v}`)
 </script>
 
 {#each items as v}
