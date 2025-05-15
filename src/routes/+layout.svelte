be2ce22732cb7a7d1618e818ff3d81c5ec78b9b0
noy4 <naoyan418@gmail.com>
2025-05-15 16:06:47 +0900

    title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,18 @@
   import 'virtual:uno.css'
 
   const { children } = $props()
+
+  const title = 'Miyajima Tide'
+  const description = 'Shows the tide information for Miyajima, Japan.'
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta property='og:title' content={title} />
+  <meta property='og:description' content={description} />
+  <meta property='og:type' content='website' />
+  <meta property='og:url' content='https://noy4.github.io/miyajima-tide/' />
+  <meta property='og:image' content='https://noy4.github.io/miyajima-tide/favicon.png' />
+</svelte:head>
 
 {@render children?.()}
